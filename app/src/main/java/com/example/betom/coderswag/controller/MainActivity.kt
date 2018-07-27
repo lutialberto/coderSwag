9ab895677cef32218b2c991cb00516551fb58e2c
Alberto Luti <betomdq.93@gmail.com>
2018-07-27 12:23:57 -0300

    list of categories is visible now

--- app/src/main/java/com/example/betom/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/betom/coderswag/controller/MainActivity.kt
@@ -4,21 +4,20 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.example.betom.coderswag.R
+import com.example.betom.coderswag.adapters.CategoryAdapter
 import com.example.betom.coderswag.model.Category
 import com.example.betom.coderswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                DataService.categories)
+        adapter = CategoryAdapter(this,DataService.categories)
         categoryListView.adapter = adapter
     }
 }
