69d9083c5ca291f521d6a901e27dcac03584d2de
Alberto Luti <betomdq.93@gmail.com>
2018-07-28 22:02:40 -0300

    product view terminated for normal and xxlarge sizes on portrait and land mode

--- app/src/main/java/com/example/betom/coderswag/controller/ProductsActivity.kt
+++ app/src/main/java/com/example/betom/coderswag/controller/ProductsActivity.kt
@@ -1,16 +1,38 @@
 package com.example.betom.coderswag.controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import com.example.betom.coderswag.R
 import com.example.betom.coderswag.Utilities.EXTRA_CATEGORY
+import com.example.betom.coderswag.adapters.ProductsAdapter
+import com.example.betom.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
-        val category = intent.getStringExtra(EXTRA_CATEGORY)
+        val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+        adapter = ProductsAdapter(this,DataService.getProducts(categoryType))
+
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if(orientation== Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+        val screenSize = resources.configuration.screenWidthDp
+        if(screenSize > 720 )
+            spanCount = 4
+
+        val layoutManager = GridLayoutManager(this,spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter=adapter
     }
 }
